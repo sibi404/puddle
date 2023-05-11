ab012b78e92276462afafd5e49debf5665f3dbe8
sibi <sibikottukandiyil2@gmail.com>
2023-05-11 21:36:47 +0530

    Signup completed

---
+++ core/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Enter usernme',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder':'Enter email',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter password',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm password',
+        'class':'w-full py-4 px-6 rounded-xl'
+    }))
--- core/views.py
+++ core/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from item.models import Category,Item
+from . forms import SignupForm
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    context = {'items':items,'categories':categories}
+    return render(request,'index.html',context)
 
 def contact(request):
     return render(request,'contact.html')
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+
+    return render(request,'signup.html',{'form':form})
