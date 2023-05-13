6679b8c0b3fde54f2ed6ebf866d98161da8c05e4
sibi <sibikottukandiyil2@gmail.com>
2023-05-13 18:51:34 +0530

    Completed dashboard

--- puddle/urls.py
+++ puddle/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
-    path('item/',include('item.urls'))
+    path('item/',include('item.urls')),
+    path('dashboard/',include('dashboard.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
