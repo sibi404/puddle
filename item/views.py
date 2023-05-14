5f582a8d5ade27f2c902ad2c467b420b9a0a73d0
sibi <sibikottukandiyil2@gmail.com>
2023-05-14 15:08:13 +0530

    Completed till deleting items

--- item/views.py
+++ item/views.py
@@ -27,3 +27,11 @@ def new(request):
     return render(request,'form.html',{
         'form':form
     })
+
+
+@login_required
+def delete_item(request,pk):
+    item = get_object_or_404(Item,pk=pk,created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
